252c81b92cc24e299b4ddb35c81b50d4d647108a
Raj Shinde <raj.shinde@viabotinc.com>
2025-08-19 17:08:27 -0700

    Add enum for discretization method

--- algorithm/ilqr/vehicle_model.h
+++ algorithm/ilqr/vehicle_model.h
@@ -16,28 +16,24 @@ using Control = Eigen::Matrix<double, kControlNum, 1>;
 using SystemMatrix = Eigen::Matrix<double, kStateNum, kStateNum>;
 using InputMatrix = Eigen::Matrix<double, kStateNum, kControlNum>;
 
+enum class DiscretizationType{
+  kRK2,
+  kForwardEuler
+};
+
 class VehicleModel {
  public:
   VehicleModel() = default;
 
-  VehicleModel(
-    const IlqrConfig& config,
-    const VehicleParam& param,
-    const double horizon, 
-    const double dt);
-
-  void DynamicsJacbian(const State& state, const Control& control, SystemMatrix* const A, InputMatrix* const B);
+  VehicleModel(const IlqrConfig& config, const VehicleParam& param, const double horizon, const double dt);
 
   void Dynamics(const State& state, const Control& control, State* const next_state);
 
-  void DynamicsFE(const State& state, const Control& control, State* const next_state);
-  
-  void DynamicsJacbianFE(const State& state, const Control& control, SystemMatrix* const A, InputMatrix* const B);
+  void DynamicsJacbian(const State& state, const Control& control, SystemMatrix* const A, InputMatrix* const B);
 
  private:
 
-  State DynamicsContinuous(
-      const State& state, const Control& control);
+  State DynamicsContinuous(const State& state, const Control& control);
 
  private:
   double horizon_;
@@ -46,7 +42,7 @@ class VehicleModel {
   IlqrConfig config_;
   VehicleParam param_;
   double k = 0.0001;
-
+  DiscretizationType discretization_type_ = DiscretizationType::kRK2;
 };
   
 } // namespace trajectory_planning
